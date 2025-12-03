77b6eb8effdea4f33dfeb2515521b27eccbe43e8
toutatsu <toutatsu.00223+git@gmail.com>
2025-12-03 15:54:58 +0900

    add: FileSystemBackend

--- src/llm/agent/deep_agent.py
+++ src/llm/agent/deep_agent.py
@@ -1,5 +1,6 @@
 # https://docs.langchain.com/oss/python/deepagents/overview
 from deepagents import create_deep_agent
+from deepagents.backends import FilesystemBackend
 
 from llm.chat_models import get_chat_model
 from llm.tools.internet_search import perform_google_search
@@ -11,6 +12,10 @@ def get_deep_agent():
         model=get_chat_model(),
         tools=[perform_google_search],
         system_prompt="system prompt",
+        backend=FilesystemBackend(
+            root_dir="/home/llm/data/agent_filesystem/",
+            virtual_mode=True,
+        ),
     )
     return deep_agent
 
